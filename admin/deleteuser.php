ffebdfba40813433966d23a2889ff0f5eeeb150a
monuruhela21 <66027443+Monu-Ruhela@users.noreply.github.com>
2021-09-06 17:13:19 +0530

    Delete admin directory

--- admin/deleteuser.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$id = $_GET['id'];
-include "dbconfigure.php";
-$query = "delete from user where emailid='$id'";
-my_iud($query);
-header("location:viewusers.php");
-?>
--- admin/deletevideo.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$id = $_GET['id'];
-include "dbconfigure.php";
-$query = "delete from video where id=$id";
-my_iud($query);
-header("location:viewallvideo.php");
-?>
